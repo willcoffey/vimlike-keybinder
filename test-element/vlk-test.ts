cdca0a1c1fa2806fc1114ad37d8186bdbdbd85ae
Will <mail@wilcof.com>
2026-01-09 17:40:12 -0500

    Revert "Refactored macros to use register-select mode, but it made execution path convoluted to follow. Need to review that"
    I believe there are some bugs related to rebinding keys during macro
    replay and buffering user input. Need to address that before merging
    into main
    
    This reverts commit 726bd9628ca912e10fde24054168fbe497fac32c.

--- test-element/vlk-test.ts
+++ test-element/vlk-test.ts
@@ -62,7 +62,7 @@ class VlkTest extends HTMLElement {
     });
 
 
-    this.vlk.takeAction("macro-register-select", "w");
+    this.vlk.takeAction("vlk-macro-replay", "w");
   }
 
   /**
@@ -165,6 +165,7 @@ class VlkTest extends HTMLElement {
   }
 
   render() {
+    console.log("Render");
     this.enumerateCurrentBindings();
     try {
       const cursor = this.getOrThrow("cursor");
